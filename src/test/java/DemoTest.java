4f9393ed30d43d157c6c563d3a8f9209462b58e1
sakshi <sakshi.mandrawal@gmail.com>
2020-01-26 23:34:11 +0530

    my first initail commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoTest {
+
+	@Test
+	public void dologin()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://support.tricentis.com/community/webshop.do");
+	//	driver.findElement(By.xpath("//*[@id=\"dropzone0_header\"]/div/div[5]/div/div/div[3]/a")).click();
+		//driver.findElement(By.xpath("//*[@id=\"user_name\"]")).sendKeys("sakshi");
+		
+		
+	}
+	
+}
